d8ecb15bd5a45f958b87b81d94ad11bd7e64b1e0
hassan <hassanpudineh67@gmail.com>
2021-09-06 23:15:08 +0430

    Add keyup handler in todo input component.

--- account/src/components/TodoInput.vue
+++ account/src/components/TodoInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex justify-items-center justify-center">
-    <input class="shadow appearance-none border rounded w-50 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="todo" placeholder="Write your todo ... " />
+    <input @keyup.enter="handleAddTodo(todo)" class="shadow appearance-none border rounded w-50 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="todo" placeholder="Write your todo ... " />
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" @click="handleAddTodo(todo)">Submit todo</button>
   </div>
 </template>
